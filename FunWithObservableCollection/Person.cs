c34c36d0013c70023bcc50bece8a64431deb2356
Osazee Isere <oz1.concepts@gmail.com>
2021-07-03 09:20:28 +0100

    initial  commit

---
+++ FunWithObservableCollection/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithObservableCollection
+{
+   public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        //custom constructor
+        public Person(string firstName,string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        public Person(){}
+
+        //methods
+        public override string ToString()=>$"[FirstName: {FirstName}, LastName: {LastName}," +
+            $"Age: {Age}]";//expresion-bodied member
+        
+        
+    }
+}
--- FunWithObservableCollection/Program.cs
+++ FunWithObservableCollection/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace FunWithObservableCollection
 {
@@ -10,6 +12,55 @@ namespace FunWithObservableCollection
     {
         static void Main(string[] args)
         {
+            //make a collection to observe and add new person object
+            ObservableCollection<Person> people = new ObservableCollection<Person>();
+            {
+                new Person { FirstName = "Jason", LastName = "Isere", Age = 11 };
+                new Person { FirstName = "Joan", LastName = "Isere", Age = 8 };
+                new Person { FirstName = "Osas", LastName = "Isere",Age= 12 };
+
+            };
+            people.Add(new Person { FirstName = "Joy", LastName = "Jacob", Age = 3 });
+
+            //wire up the collection change
+            people.CollectionChanged += people_CollectionChanged;
+            Console.ReadLine();
+        }
+
+        static void people_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            //what was the action that caused the event
+            Console.WriteLine("Action for this event {0}", e.Action);
+
+            //they removed something
+            if(e.Action==System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                Console.WriteLine("Here are the old items");
+                foreach(Person p in e.OldItems)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+                Console.WriteLine();
+
+                //they added something
+                if(e.Action==System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                {
+                    foreach(Person p in e.NewItems)
+                    {
+                        Console.WriteLine(p.ToString());
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        public enum NotifyCollectionChangedAction
+        {
+            Add=0,
+            Remove=1,
+            Replavce=2,
+            Move=3,
+            Reset=4,
         }
     }
 }
